9c61400b09e02c81e5a330d249c906b1e3e346be
grantwbrooks <grantwbrooks@gmail.com>
2017-09-11 19:07:26 -0400

    adding to beast finished

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
+    
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        
+        if (taskTextField.text?.characters.count)! > 0 {
+            tasks.append(taskTextField.text!)
+            print(tasks)
+            taskTextField.text = ""
+            tableView.reloadData()
+        }
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +42,24 @@ class ViewController: UIViewController {
     }
 
 
+}
+extension ViewController: UITableViewDataSource {
+    // MAKE SURE THESE ARE WITHIN UITableViewDataSource EXTENSION!
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return an integer that indicates how many rows (cells) to draw
+        return tasks.count
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get the UITableViewCell and create/populate it with data then return it
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        // set the text label
+        cell.textLabel?.text = tasks[indexPath.row]
+        // be sure to return the cell
+        return cell
+    }
 }
 
